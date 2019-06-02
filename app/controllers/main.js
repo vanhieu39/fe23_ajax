2bed802486cd06666e4b6c399b4c4a175919b3ce
vanhieu39 <bhhuyenbi@gmail.com>
2019-06-02 16:35:37 +0700

    update tim kiem vao main

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -8,7 +8,11 @@ $(document).ready(function() {
   };
   function taoBranchGit_02_06_2019(){
     
-  }
+  };
+
+  function timkiem(){
+
+  };
 
   function layDanhSachNguoiDung() {
     danhSachNguoiDung
